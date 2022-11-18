c9b9c33173a2570c7fb91e0ed767d5b7413e0afd
focus2751 <79010243+focus2751@users.noreply.github.com>
2022-11-18 19:34:47 +0530

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,55 @@
+objectDetector = "";
+    img = "";
+    objects = [];
+    status = "";
+
+    function preload()
+{
+img = loadImage('laptop.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image( img, 0, 0, 640, 420);
+    if(status != "")
+    {  
+   
+      for (var i = 0; i < objects.length; i++)
+      {
+        document.getElementById("status").innerHTML = "Status: Object detected";
+        fill(234, 0, 0);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+        noFill();
+        stroke(234, 0, 0);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
+
+  
+}
